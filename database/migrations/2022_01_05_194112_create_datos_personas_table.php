b87a868d10be6e9a436ccfc47f37e519820f3b39
nombre_usuario_github <pipos_rgt@hotmail.com>
2022-01-05 15:00:22 -0500

    migration datos persona

---
+++ database/migrations/2022_01_05_194112_create_datos_personas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDatosPersonasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('datos_personas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('cedula');
+            $table->string('direccion');
+            $table->integer('id_tipo_personas');
+            $table->integer('id_horario_atencion');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('datos_personas');
+    }
+}
