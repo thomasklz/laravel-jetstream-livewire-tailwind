38f9912515158668a87437b0993c13dcc024341b
nombre_usuario_github <pipos_rgt@hotmail.com>
2022-01-08 17:57:10 -0500

    insert show

--- app/Http/Livewire/Persona.php
+++ app/Http/Livewire/Persona.php
@@ -11,9 +11,29 @@ class Persona extends Component
   
     public $nombre, $apellido, $cedula, $direccion, $telefono;
      
+
+protected $rules=[
+    'nombre'=>'required',
+    'apellido'=> 'required',
+    'cedula'=>'required|numeric|min:10',
+    'direccion'=> 'required',
+    'telefono'=>'required ',
+];
+public function updated($propertyName)
+{
+    $this->validateOnly($propertyName);
+}
+protected $messages = [
+    'nombre.required' => 'El campo nombre es obligatorio',
+    'cedula.required' => 'El campo cédula es obligatorio',
+    'cedula.numeric' => 'Solo números',
+    'cedula.min' => 'En el campo cédula minímo son 10 carñacteres',
+];
+
     public function guardar(){
-       
- 
+
+      $this->validate();
+
        $persona= new DatosPersona();
        $persona->nombre=$this->nombre;
        $persona->apellido=$this->apellido;
@@ -23,12 +43,21 @@ class Persona extends Component
        $persona->id_tipo_personas=1;
        $persona->id_horario_atencion=1;
        $persona->save();
+       session()->flash('message', 'Persona registrada correctamente');
+       $this->limpiar();
    }
     public function render()
     {
-        return view('livewire.persona');
+        $personas=DatosPersona::all();
+        return view('livewire.persona',['personas'=>$personas]);
     }
-
+ public function limpiar(){
+    $this->nombre="";
+    $this->apellido="";
+    $this->cedula="";
+    $this->direccion="";
+    $this->telefono="";
+ }
 
     
 }
---
+++ resources/views/mensajes/success.blade.php
@@ -0,0 +1,9 @@
+<div>
+    @if (session()->has('message'))
+        <div class="p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800" role="alert">
+          <span class="font-medium">
+           Correcto!</span> {{ session('message') }}
+        </div>
+
+    @endif
+</div>
